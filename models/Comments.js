9fcb5a002108b7cf708f5370aac275355fdfa65c
AdamR-Work <aramosatwork@hotmail.com>
2021-03-22 16:49:57 -0500

    models started, index file for models started.

---
+++ models/Comments.js
@@ -0,0 +1,40 @@
+const {Model, Datatypes} = require('sequelize');
+const sequelize = require('../config/connection')
+
+class Comments extends Model{};
+Comments.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      comment_text: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      steps_id:{
+        type: Datatypes.INTEGER,
+        references:{
+            model:'steps',
+            key: 'id'
+        }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'comments'
+    }
+  );
+
+module.exports = Comments;
