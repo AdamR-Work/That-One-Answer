a631cec3ef26f5855a348bc5d6c184a58a1f481e
Adam Ramos <74208516+AdamR-Work@users.noreply.github.com>
2021-03-27 15:26:21 -0500

    Revert "Feature/login updates"

--- controllers/api/comment.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const withAuth = require('../../utils/auth');
-
-const {Answer, User, Comments, Category} = require('../../models');
-
-module.exports = router;
--- db/schema.sql
+++
@@ -1,5 +0,0 @@
-
-
-DROP DATABASE IF EXISTS that_one_answer_db;
-
-CREATE DATABASE that_one_answer_db;
