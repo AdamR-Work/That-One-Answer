18698a3776a446593b14e38a64e4e1fcb3881545
Rolanduwxcc <74523466+rolanduwxcc@users.noreply.github.com>
2021-03-27 12:29:09 -0500

    Updated the User model to support logging in!

--- models/User.js
+++ models/User.js
@@ -1,8 +1,15 @@
 
 const {Model, DataTypes} = require('sequelize');
-const sequelize = require('../config/connection')
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+const { log } = require('handlebars');
 
-class User extends Model{}
+class User extends Model{
+
+  checkPassword(loginPassword) {
+    return bcrypt.compareSync(loginPassword, this.password);
+  }
+}
 
 User.init(
     {
@@ -34,7 +41,24 @@ User.init(
         }
     },
     {
-        
+      hooks: {
+        //hash a password before storing it
+        // beforeCreate(userData) {
+        //     return bcrypt.hash(userData.password, 10).then(hashedPassword => {
+        //         newUserData.password = hashedPassword;
+        //         return newUserData;
+        //     });
+        // }
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        //set up beforeUpdate hook
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+      },
         sequelize,
         timestamps: false,
         freezeTableName: true,
