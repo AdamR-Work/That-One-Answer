fcf1931f64a98d197009a6b351b8a707be3d1396
Rolanduwxcc <74523466+rolanduwxcc@users.noreply.github.com>
2021-03-30 16:26:28 -0500

    New answers isn't working now

--- public/javascript/create-answer.js
+++ public/javascript/create-answer.js
@@ -1,16 +1,12 @@
 //Creates the Answer page with the title and description
-async function addPage(event) {
-
+async function addPage(event){
   event.preventDefault();
-  const title = document.querySelector('#new-answer-title').value;//
-  const description = document.querySelector('#new-answer-description').value;
-  const category_id = document.querySelector('#new-answer-category').value;
-
-  console.log(title + description + category_id);
+  const title = document.querySelector('#new-answer-category').value;
+  const description = document.querySelector('#new-answer-title').value;
+  const category_id = document.querySelector('#new-answer-description').value;
   
-  // ADD THE Page.data via the route
-  if (title && description && category_id) {
-    const response = await fetch('/api/answer', {
+  // ADD THE Page.data via the route 
+  const response = await fetch('/api/answer',{
       method: 'POST',
       body: JSON.stringify({
         title,
@@ -23,36 +19,34 @@ async function addPage(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      const newAnswer = await response.json();
+      const newId = newAnswer.id;
+
+      document.location.replace('/answer/'+newId);
     } else {
       alert(response.statusText);
     }
-  } else {
-    alert("Oh no, something went wrong! You better call somebody!");
-  }
-}
+  }  
 
-
-
-document.querySelector('#new-answer-btn').addEventListener('submit', addPage);
+  document.querySelector('.new').addEventListener('submit', addPage);
 
 // attempt 4
-     // const location = newAnswer.id;
-    //  if (response.ok) {//trying to get it to create page after assigning title& description
-    //      const newAnswer = await response.json();
-
-    //     const newID = newAnswer.id;
-    //     console.log(newID)
-    //     res.render('/answer/'+newID)
-    //   } else {
-    //     // console.log(response)
-    //  -   alert(response.statusText);
-    //   }
+   // const location = newAnswer.id;
+  //  if (response.ok) {//trying to get it to create page after assigning title& description
+  //      const newAnswer = await response.json();
+  
+  //     const newID = newAnswer.id;
+  //     console.log(newID)
+  //     res.render('/answer/'+newID)
+  //   } else {
+  //     // console.log(response)
+  //  -   alert(response.statusText);
+  //   }
 
 
 // attempt 6
-    // .then(dbnewAnswer => {
-    //   const answer = dbnewAnswer.map(answer => answer.get({plain:true}));
-    //   console.log(answer);
-    //   res.render('answer', answer);
-    // })
+  // .then(dbnewAnswer => {
+  //   const answer = dbnewAnswer.map(answer => answer.get({plain:true}));
+  //   console.log(answer);
+  //   res.render('answer', answer);
+  // })
